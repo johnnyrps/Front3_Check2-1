985fe6a0ef6e0ae436f7a24065ea954a31ee60f1
johnnyrps <johnnyrps@gmail.com>
2022-12-14 20:24:22 -0300

    impl_card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,26 +1,26 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ContextGlobal } from "./utils/global.context";
 import styles from "./Card.module.css";
 
-const Card = () => {
+const Card = ({ nome, matricula, usuario }) => {
+  const { theme } = useContext(ContextGlobal);
+  const isDarkMode = theme === "dark" || false;
 
   return (
-    <>
-      {/* //Na linha seguinte deverá ser feito um teste se a aplicação
-        // está em dark mode e deverá utilizar o css correto */}
-      <div className={`card`}>
-        <img
-          className="card-img-top"
-          src="/images/doctor.jpg"
-          alt="doctor placeholder"
-        />
-        <div className={`card-body ${styles.CardBody}`}>
-          {/* Na linha seguinte o link deverá utilizar a matricula, nome e sobrenome do dentista
-          que vem da API */}
-          <a href={`/dentist/MatriculaDoDentista`}>
-            <h5 className={`card-title ${styles.title}`}>Nome e Sobrenome do dentista</h5>
-          </a>
-        </div>
+    <div className={`card ${isDarkMode ? styles.cardDark : ""}`}>
+      <img
+        className="card-img-top"
+        src="/images/doctor.jpg"
+        alt="doctor placeholder"
+      />
+      <div className={`card-body ${styles.CardBody}`}>
+        <Link to={`/dentist/${matricula}`}>
+          <h5 className={`card-title ${styles.title}`}>{nome}</h5>
+        </Link>
+        <p className="card-text">{usuario?.username}</p>
       </div>
-    </>
+    </div>
   );
 };
 
