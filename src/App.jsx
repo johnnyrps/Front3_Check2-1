812078eaa0d000470349ddf7293e19f580d086e5
johnnyrps <johnnyrps@gmail.com>
2022-12-14 01:05:20 -0300

    14-12-2022-johnny

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 
-import { Outlet } from "react-router-dom";
+import { useEffect, useContext } from 'react';
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import { ContextGlobal } from './Components/utils/global.context';
+
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { theme } = useContext(ContextGlobal)
+  const isDarkMode = theme === "dark" || false;
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      navigate("/home");
+    }
+  });
+
   return (
     <>
       {/* //Na linha seguinte deverá ser feito um teste se a aplicação
         // está em dark mode e deverá utilizar a classe dark ou light */}
-      <div className={`app light}`}>
+      <div className={`app ${isDarkMode ? "dark" : "light"}`}>
         <Navbar />
         <main>
           <Outlet />
--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,9 +1,17 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Card from "../Components/Card";
 
 const Home = () => {
+  const [dentist, setDentist] = useState([]);
 
   useEffect(() => {
+    try {
+      fetch("https://dhodonto.ctdprojetos.com.br/dentista")
+        .then((res) => res.json())
+        .then((data) => setDentist(data));
+    } catch (error) {
+      console.log(error);
+    }
     //Nesse useEffect, deverá ser obtido todos os dentistas da API
     //Armazena-los em um estado para posteriormente fazer um map
     //Usando o componente <Card />
@@ -13,7 +21,10 @@ const Home = () => {
     <>
       <h1>Home</h1>
       <div className="card-grid container">
-        <Card />
+        {dentist.length ? dentist.map((dentist) => (
+            <Card {...dentist} key={dentist.matricula} />
+          ))
+        : null}
       </div>
     </>
   );
