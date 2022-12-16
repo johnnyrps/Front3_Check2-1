81bfe5c0499dd9a1f0293adb345ccb55aa956a25
johnnyrps <johnnyrps@gmail.com>
2022-12-15 23:09:08 -0300

    ok

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Home from "./Routes/Home";
 import Login from "./Routes/Login";
